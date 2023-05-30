eed52c6bab6d4d48f68173ca500f6b03af797e01
sabujsarker382~ <raj16@cse.pstu.ac.bd>
2023-05-30 09:54:49 +0600

    Undefined

---
+++ undefined vs null.js
@@ -0,0 +1,13 @@
+// let nam;
+// console.log(nam);
+// function add(num1,num2) {
+//     console.log(num1+num2);
+    
+// }
+// function add(num1,num2){
+//     console.log(num1,num2);
+// }
+const sabuj={age:"23",phone:"222323",bou:"piu"};
+
+
+console.log(sabuj.work);
