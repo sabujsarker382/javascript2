248bdf67ccd66b768947ed07f5fe73213c869955
sabujsarker382~ <raj16@cse.pstu.ac.bd>
2023-05-30 10:03:42 +0600

    === vs ==

---
+++ triple equal.js
@@ -0,0 +1,10 @@
+const n=12;
+const n1="12";
+if(n===n1){    //===check number +datatype
+               // ==check only number not data type
+    console.log("True");
+    
+}
+else{
+    console.log("false");
+}
